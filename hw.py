82da3b69ea4534ac5e267b907247377dc3572c76
Ishaan Srivastav <ishaan2014@gmail.com>
2019-10-13 23:23:12 +0530

    done

---
+++ hw.py
@@ -0,0 +1,32 @@
+import cv2
+
+face_cascades = cv2.CascadeClassifier('/home/ishaan/Documents/py/CV/Module 1 - Face Recognition/haarcascade_frontalface_default.xml')
+eye_cascades = cv2.CascadeClassifier('/home/ishaan/Documents/py/CV/Module 1 - Face Recognition/haarcascade_eye.xml')
+smile_cascades = cv2.CascadeClassifier('/home/ishaan/Documents/py/CV/019 Homework/haarcascade_smile.xml')
+
+
+
+def detect(gray, frame):
+    faces = face_cascades.detectMultiScale(gray, 1.3, 5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame [y:y+h, x:x+w]
+        eyes = eye_cascades.detectMultiScale(roi_gray, 1.1, 22)
+        for(ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
+        smiles = smile_cascades.detectMultiScale(roi_gray, 1.8, 28)
+        for(sx,sy,sw,sh) in smiles:
+            cv2.rectangle(roi_color, (sx,sy), (sx+sw,sy+sh), (0,0,255),2)
+    return frame
+
+video_capture = cv2.VideoCapture(0)
+while True:
+    _,frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray,frame)
+    cv2.imshow('Video', canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
